d7e56bd807c745c6a3a4c1d72a47937558a2bba4
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-04 17:27:41 +0300

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  // const myTitle = "Ahmet Demir";
+  const [state, setState] = useState("Ahmet Demir");
+
+  const handleTitleChange = () => {
+    console.log("run");
+    setState("Emin Başbayan");
+  };
+
   return (
     <>
-      <h1>Hello React!</h1>
-      <button>Ismi Değiştir</button>
+      <h1>{state}</h1>
+      <button onClick={handleTitleChange}>İsmi Değiştir</button>
     </>
   );
 };
