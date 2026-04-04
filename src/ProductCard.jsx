1adae306a37db187e9ea912e2f0a954fa61e9259
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-04 17:51:35 +0300

    component inline css

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <div className="product-card">
+    <div
+      className="product-card"
+      style={{
+        backgroundColor: "red",
+        color: "white",
+      }}
+    >
       <img
         src="https://konyaberraksu.com/wp-content/uploads/2019/03/5lt-buyuk.png"
         alt=""
