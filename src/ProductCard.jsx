3684be2ce08e04d4f813ed38c0e69984eea4ffd7
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-05 15:57:16 +0300

    componentlerde js degisken kullanımı

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import "./ProductCard.css";
 
 const ProductCard = () => {
+  const image =
+    "https://konyaberraksu.com/wp-content/uploads/2019/03/5lt-buyuk.png";
+  const title = "Büyük Su";
+  const price = 20;
+
   return (
     <div className="product-card">
-      <img
-        className="product-image"
-        src="https://konyaberraksu.com/wp-content/uploads/2019/03/5lt-buyuk.png"
-        alt=""
-      />
+      <img className="product-image" src={image} alt="" />
       <div className="product-info">
-        <strong className="product-name">Büyük Su</strong>
-        <span className="product-price">20₺</span>
+        <strong className="product-title">{title}</strong>
+        <span className="product-price">{price}₺</span>
       </div>
     </div>
   );
