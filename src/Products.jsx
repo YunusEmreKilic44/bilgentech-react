d3286db3379407730843a7cad2be85afb9bccc67
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-05 17:53:46 +0300

    component key özelliği

--- src/Products.jsx
+++ src/Products.jsx
@@ -5,10 +5,6 @@ import "./Products.css";
 
 // Ürünlerle ilgili ana parent component
 const Products = () => {
-  const image =
-    "https://konyaberraksu.com/wp-content/uploads/2019/03/5lt-buyuk.png";
-  const title = "Büyük Su";
-  const price = 20;
   return (
     <div className="products">
       <h2>Products Component</h2>
