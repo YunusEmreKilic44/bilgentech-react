41bc1da22b58ea6d13371b6a8fe58b369a4e7355
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-09 23:10:24 +0300

    tekli state ve tekli function ile form yönetimi

--- src/components/Products/AddProductForm.jsx
+++ src/components/Products/AddProductForm.jsx
@@ -3,59 +3,54 @@ import Button from "../UI/Button";
 import "./AddProductForm.css";
 
 const AddProductForm = () => {
-  const [title, setTitle] = useState("");
-  const [image, setImage] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
+  const [product, setProduct] = useState({
+    title: "",
+    image: "",
+    price: 0,
+    description: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleImageChange = (e) => {
-    setImage(e.target.value);
-  };
-
-  const handlePriceChange = (e) => {
-    setPrice(e.target.value);
-  };
-
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProduct((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
     <form className="add-product-form">
       <label>
-        Title: {title}
+        Title: {product.title}
         <input
           type="text"
           placeholder="Bir ürün ismi giriniz!"
-          onChange={handleTitleChange}
+          onChange={handleChange}
+          name="title"
         />
       </label>
       <label>
-        Image URL: {image}
+        Image URL: {product.image}
         <input
           type="text"
           placeholder="Bir ürün görseli giriniz!"
-          onChange={handleImageChange}
+          onChange={handleChange}
+          name="image"
         />
       </label>
       <label>
-        Price: {price}
+        Price: {product.price}
         <input
           type="number"
           placeholder="Bir ürün fiyatı giriniz!"
-          onChange={handlePriceChange}
+          onChange={handleChange}
+          name="price"
         />
       </label>
       <label>
-        Description: {description}
+        Description: {product.description}
         <input
           type="text"
           placeholder="Bir ürün açıklaması giriniz!"
-          onChange={handleDescriptionChange}
+          onChange={handleChange}
+          name="description"
         />
       </label>
       <Button variant="primary">Ürünü Ekle</Button>
