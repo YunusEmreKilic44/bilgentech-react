03976787a9193edfcd88a58d97f29d7fddc152e0
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-09 22:45:12 +0300

    addProductForm

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,12 +1,14 @@
 import ProductCard from "./ProductCard";
 import productsData from "../../productsData";
 import "./Products.css";
+import AddProductForm from "./AddProductForm";
 
 // Ürünlerle ilgili ana parent component
 const Products = () => {
   return (
     <div className="products">
       <h2>Products Component</h2>
+      <AddProductForm />
       <div className="products-wrapper">
         {productsData.map((product) => (
           <ProductCard
