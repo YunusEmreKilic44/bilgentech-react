57075829c9c5251c40edcaeab6afe29ee455e5e6
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-13 18:49:06 +0300

    modal component yapısı - lucide react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import ProductCard from "./components/Products/ProductCard";
 import Products from "./components/Products/Products";
 import Button from "./components/UI/Button";
+import Modal from "./components/UI/Modal";
 
 const App = () => {
   return (
     <>
       <h1>App Component</h1>
-      <Button title="1. Component" variant="primary" /> {/* 1. Component */}
-      <Button title="2. Component" variant="secondary" /> {/* 2. Component */}
-      <Button title="3. Component" variant="success" /> {/* 3. Component */}
-      <Button title="Sil" variant="danger" size="sm" />
-      <Button title="Ürün Ekle" variant="primary" size="lg" addClass="hidden" />
-      <Button variant="secondary" size="lg">
-        <b>
-          Deneme Buton
-          <span>asdas</span>
-        </b>
-      </Button>
+
       <Products />
+      <Modal />
     </>
   );
 };
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,30 @@
+import "./Modal.css";
+import { CircleX } from "lucide-react";
+
+const Modal = () => {
+  return (
+    <div className="modal fade">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5">Modal Title</h1>
+            <button type="button" className="btn-close">
+              <CircleX />
+            </button>
+          </div>
+          <div className="modal-body">description</div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary">
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
