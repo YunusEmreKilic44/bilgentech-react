13a274c8da33b7c9f387676a7ec3bb8fc5630ea4
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-21 12:35:16 +0300

    react-router v7 kurulumu

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import Counter from "./components/Counter";
 import ProductCard from "./components/Products/ProductCard";
 import Products from "./components/Products/Products";
 import Button from "./components/UI/Button";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
+
   return (
     <div className="p-4">
-      <h1>App Component</h1>
+      <RouterProvider router={router} />
       {/* <LoginPage /> */}
-      <RegisterPage />
+      {/* <RegisterPage /> */}
       {/* <Products /> */}
       {/* <Counter /> */}
     </div>
